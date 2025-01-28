0375ea241c030e93eec01a45432995809409239f
Cristin Cioara <cristin.cioara@gmail.com>
2025-01-27 20:08:19 -0600

    configured dynamodb,s3bucket,lamda,ecr

--- terraform/lamda-module/outputs.tf
+++ terraform/lamda-module/outputs.tf
@@ -1,3 +1,13 @@
 output "api_endpoint" {
   value = aws_api_gateway_deployment.deployment.invoke_url
 }
+
+output "lambda_id" {
+  description = "The ID of the Lambda function"
+  value       = aws_lambda_function.lambda.id
+}
+
+output "lambda_arn" {
+  description = "The ARN of the Lambda function"
+  value       = aws_lambda_function.lambda.arn
+}
--- terraform/s3-module/outputs.tf
+++ terraform/s3-module/outputs.tf
@@ -2,8 +2,3 @@ output "bucket_name" {
   description = "The name of the S3 bucket"
   value       = aws_s3_bucket.static_website.id
 }
-
-output "website_endpoint" {
-  description = "The website endpoint for the S3 bucket"
-  value       = aws_s3_bucket.static_website.website_endpoint
-}
