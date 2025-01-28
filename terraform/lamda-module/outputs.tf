9b605e5a8f2089734f7bc4cd037480d33781c610
Cristin Cioara <cristin.cioara@gmail.com>
2025-01-27 20:13:07 -0600

    configured dynamodb,s3bucket,lamda,ecr

--- terraform/lamda-module/outputs.tf
+++ terraform/lamda-module/outputs.tf
@@ -2,12 +2,12 @@ output "api_endpoint" {
   value = aws_api_gateway_deployment.deployment.invoke_url
 }
 
-output "lambda_id" {
+output "lamda_id" {
   description = "The ID of the Lambda function"
   value       = aws_lambda_function.lambda.id
 }
 
-output "lambda_arn" {
+output "lamda_arn" {
   description = "The ARN of the Lambda function"
   value       = aws_lambda_function.lambda.arn
 }
