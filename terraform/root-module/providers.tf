8139a6c688cc209c68d573cec72b9c6ed6fd203d
Cristin Cioara <cristin.cioara@gmail.com>
2025-01-27 19:46:14 -0600

    configured dynamodb,s3bucket,lamda,ecr

--- terraform/root-module/providers.tf
+++ terraform/root-module/providers.tf
@@ -8,10 +8,10 @@ terraform {
     
 
   backend "s3" {
-    bucket = "serverless24b-redhat-statefile-bucket"
+    bucket = "terraform-state-nscc77"
     key    = "memo-app.tfstate"
     region = "us-east-1"
-    dynamodb_table = "serverless24b-redhat-dev-terraform-lock"
+    dynamodb_table = "terraform-lock-table"
   }
 }
 
